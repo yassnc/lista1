e274b57dc35e88db946c54b95e2a43641e048a15
Yasmin Nunes <yasmin.nunes@aluno.ifsp.edu.br>
2025-04-16 20:11:46 -0300

    Add files via upload

---
+++ ex1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	int base;
+	int altura;
+	int area;
+	
+	cout << "informe a base do retangulo: ";
+	cin >> base;
+	
+	cout << "informe a altura do retangulo: ";
+	cin >> altura;
+	
+	area = base * altura;
+	
+	cout << "A area do retangulo es " << area << endl;
+}
