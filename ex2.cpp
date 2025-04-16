12c4917d6ec497a04ff48d4eb7990ab901c49d70
Yasmin Nunes <yasmin.nunes@aluno.ifsp.edu.br>
2025-04-16 20:24:44 -0300

    Add files via upload

---
+++ ex2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	int aresta;
+	
+	int area;
+	
+	cout << "informe o valor da aresta a: ";
+	cin >> aresta;
+	
+
+	area = aresta * aresta;
+	
+	cout << "a area do quadrado es " << area << endl; 
+	
+	return 0;
+}
